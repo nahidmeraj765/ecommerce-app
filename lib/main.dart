2855fdc0fc6b77cda2dae7fc913c8f20aa057731
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2025-12-25 22:44:31 +0600

    firebase setup, adding crashlytics and analytics

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,27 @@
 // 7. Routing
 // 8. Network Caller
 
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_ecommerce/app/my_app.dart';
+import 'package:flutter_application_ecommerce/firebase_options.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+    
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+    runApp(MyApp());
 }
